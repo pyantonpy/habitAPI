c792d08428b5714491648dbc61074ceecb4751aa
pyantonpy <antonmelnikov@MacBook-Air-Anton.local>
2025-11-26 19:56:26 +0300

    try3

--- habit_tracker/main.py
+++ habit_tracker/main.py
@@ -5,5 +5,5 @@ from habit_tracker.api import habits_api
 
 app = FastAPI(title="Habit Tracker API")
 app.mount("/static", StaticFiles(directory="habit_tracker/static"), name="static")
-app.include_router(web.router)
-app.include_router(habits_api.router)
+app.include_router(web.router, tags=["Web Interface"])
+app.include_router(habits_api.router, prefix="/api/habits", tags=["Habits API"])
